770eb6e1b52b4adf8823832807502b53ec340c98
asqarbek olimov <88034288+asqarbekolimov@users.noreply.github.com>
2023-09-02 11:13:45 +0500

    ts & eslint config

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Roboto } from "@next/font/google";
+const inter = Roboto({
+  weight: ["400", "500", "700", "900"],
+  subsets: ["latin"],
+});
+
+export default function Index() {
+  return <div>Index</div>;
+}
