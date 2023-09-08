277e9c417b997bae746dfcedd8ce8e867d9d8572
asqarbek olimov <88034288+asqarbekolimov@users.noreply.github.com>
2023-09-08 16:59:27 +0500

    footer style

--- layout/footer/footer.tsx
+++ layout/footer/footer.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import { FooterProps } from "./footer.props";
+import styles from "./footer.module.css";
+import cn from "classnames";
+import { format } from "date-fns";
 
-const Footer = ({ ...props }: FooterProps): JSX.Element => {
-  return <div {...props}>Footer</div>;
+const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+  return (
+    <footer className={cn(className, styles.footer)} {...props}>
+      <div>
+        Education © 2022 - {format(new Date(), "yyyy")}. All right reserver{" "}
+      </div>
+      <a href="#">Terms of use </a>
+      <a href="#">Privacy Policy</a>
+    </footer>
+  );
 };
 
 export default Footer;
