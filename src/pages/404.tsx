9fc6df1af963c1a7dbd0296e057c2de16d3cab31
asqarbek olimov <88034288+asqarbekolimov@users.noreply.github.com>
2023-10-01 18:00:13 +0500

    404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <div
+        style={{
+          width: "100%",
+          height: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <h1 style={{ textAlign: "center" }}>404 | Not Found</h1>
+      </div>
+      ;
+    </div>
+  );
+};
+export default ErrorPage;
